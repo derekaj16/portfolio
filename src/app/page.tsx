86bb74a0a2aee41f4a4fcfaa946ac16876305be6
derekaj16 <dprodigy@byu.edu>
2025-03-14 12:50:02 -0600

    simplify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,29 @@
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px]  justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div className="text-center flex flex-col gap-4">
-          <h1 className="text-3xl text-primary">Derek Johnson</h1>
-          <div className="text-lg text-muted-foreground uppercase leading-2">
-            Portfolio
+        <div className="flex flex-col gap-8">
+          <div className="flex flex-col gap-4">
+            <div className="text-lg text-muted-foreground uppercase leading-2 font-medium">
+              Portfolio
+            </div>
+            <h1 className="text-5xl text-primary font-semibold">
+              Derek Johnson
+            </h1>
+            <div className="text-gray-400">
+              My portfolio is simple so I can focus on things that actually
+              matter.
+            </div>
+          </div>
+
+          <div>
+            <h2>Education</h2>
+          </div>
+          <div>
+            <h2>Experience</h2>
+          </div>
+          <div>
+            <h2>Hobbies/Interests</h2>
           </div>
         </div>
       </main>
