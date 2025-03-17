aa523115cdb9d2f13c3dbbef2f9bb32b8feeca63
derekaj16 <dprodigy@byu.edu>
2025-03-17 14:35:36 -0600

    adding link

--- src/app/awards/page.tsx
+++ src/app/awards/page.tsx
@@ -33,6 +33,16 @@ export default function Page() {
                 <div className="italic text-muted-foreground">
                   January 1, 2024
                 </div>
+                <div className="flex flex-row gap-3 items-center">
+                  <LinkIcon size={20} className="text-muted-foreground" />
+                  <Link
+                    href="https://www.credly.com/badges/5db50e60-0f8c-45cc-ad87-54a87048fb1f/"
+                    target="_blank"
+                    className="text-sky-500 hover:text-sky-400 transition"
+                  >
+                    credly.com/badges
+                  </Link>
+                </div>
               </div>
             </div>
             <div>
