570fcfd1f49ab146fe9f17b0d57006cf3365163b
derekaj16 <dprodigy@byu.edu>
2025-04-30 17:49:14 -0600

    changing order of projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,8 +42,8 @@ export default function Home() {
               <BeThereCard />
               <FamilySearchCard />
               <RecipeAppCard />
-              <SantaCard />
               <KidneyAid />
+              <SantaCard />
               <InsertCompanyCard />
             </div>
           </div>
