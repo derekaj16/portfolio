c1cab3263f1b85749534a134ad62e21c7d29a245
derekaj16 <derekaj16@gmail.com>
2025-10-30 13:49:30 -0600

    changes

--- src/app/components/recipe-app-card.tsx
+++ src/app/components/recipe-app-card.tsx
@@ -7,7 +7,7 @@ import {
   CardTitle,
 } from '@/components/ui/card'
 import Image from 'next/image'
-import recipesDemo from '../../../public/img/recipe.gif'
+import coconut from '../../../public/img/coconut.png'
 import { LogoCircle } from '@/components/logo-circle'
 import reactLogo from '../../../public/img/react.png'
 import tsLogo from '../../../public/img/ts.png'
@@ -19,16 +19,17 @@ const RecipeAppCard = () => {
       <CardContent className="p-0 pb-8">
         <CardHeader>
           <Image
-            src={recipesDemo}
-            className="w-full rounded-lg mb-2"
+            src={coconut}
+            className="w-full rounded-lg mb-2 max-h-48 object-contain bg-amber-50"
             alt="demo of BeThere"
           />
           <CardTitle className="text-2xl font-semibold">Recipes App</CardTitle>
           <CardDescription>Current Side Project</CardDescription>
         </CardHeader>
         <div className="p-6">
-          I&apos;m developing an OP recipes app to make my food life easier.
-          This app is not yet released but soon will be!
+          I&apos;m developing an OP recipes app to make my food life easier. The
+          app will have everything from recipe management to meal planning,
+          instant grocery lists, and AI.
         </div>
         <CardFooter className="flex flex-row justify-between items-center absolute bottom-4 w-full">
           <div className="flex flex-row gap-1">
