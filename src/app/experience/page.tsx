df06e33ce43995a7e15823ac70be1648a15fd198
derekaj16 <71414948+derekaj16@users.noreply.github.com>
2026-01-13 16:07:30 -0500

    Update page.tsx

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -21,7 +21,7 @@ export default function Page() {
             </h1>
             <div className="flex flex-col gap-24 my-8">
               <BeThere />
-              <Sunwest />
+             
               <TextAnalyzer />
               <FamilySearch />
               <MTC />
